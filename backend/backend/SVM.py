b42c943dd92b04ffbe4362c6d15a9c6120b9ee25
@Louka98 <loubna.kadri@yahoo.com>
2020-10-15 01:05:27 +0200

    SVM Classifier creation

---
+++ backend/backend/SVM.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report, confusion_matrix
+
+
+
+data = load_breast_cancer()
+X, y = load_breast_cancer(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+svclassifier = SVC(kernel='linear')
+classifier = svclassifier.fit(X_train, y_train)
+y_pred = svclassifier.predict(X_test)
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+
+def make_meshgrid(x, y, h=.02):
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax, classifier, xx, yy, **params):
+    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+fig, ax = plt.subplots()
+
+title = ('Decision surface of linear SVC ')
+# Set-up grid for plotting.
+X0, X1 = X[:, 0], X[:, 1]
+xx, yy = make_meshgrid(X0, X1)
+
+plot_contours(ax, classifier, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+ax.set_ylabel('y label here')
+ax.set_xlabel('x label here')
+ax.set_xticks(())
+ax.set_yticks(())
+ax.set_title(title)
+ax.legend()
+plt.show()
