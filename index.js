115e3d8cb696d000632329eb9553b52a03e7bc7c
Latifa Lechlech <43552035+LechlechLatifa@users.noreply.github.com>
2020-10-22 20:50:46 +0000

    Modifying the index Page

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        $(".slideanim").each(function(){
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 600) {
+                $(this).addClass("slide");
+            }
+        });
+    });
+
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+});
