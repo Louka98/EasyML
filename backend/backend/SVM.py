413d5980fb6561aac0b6ece2e77189e651668f1d
@Louka98 <loubna.kadri@yahoo.com>
2020-10-30 18:01:38 +0100

    plotting

--- backend/backend/SVM.py
+++ backend/backend/SVM.py
@@ -16,35 +16,4 @@ svclassifier = SVC(kernel='linear')
 classifier = svclassifier.fit(X_train, y_train)
 y_pred = svclassifier.predict(X_test)
 print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))
-
-def make_meshgrid(x, y, h=.02):
-    x_min, x_max = x.min() - 1, x.max() + 1
-    y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-    return xx, yy
-
-
-def plot_contours(ax, classifier, xx, yy, **params):
-    Z = classifier.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = Z.reshape(xx.shape)
-    out = ax.contourf(xx, yy, Z, **params)
-    return out
-
-
-fig, ax = plt.subplots()
- 
-title = ('Decision surface of linear SVC ')
-# Set-up grid for plotting.
-X0, X1 = X[:, 0], X[:, 1]
-xx, yy = make_meshgrid(X0, X1)
-
-plot_contours(ax, classifier, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
-ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
-ax.set_ylabel('y label here')
-ax.set_xlabel('x label here')
-ax.set_xticks(())
-ax.set_yticks(())
-ax.set_title(title)
-ax.legend()
-plt.show()
+print(classification_report(y_test,y_pred
