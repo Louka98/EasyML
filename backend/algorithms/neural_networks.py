986610b3f6b0f8e8541bac1de89646ef3abc9910
Czapp Adam <czappadam@gmail.com>
2020-11-03 16:46:50 +0100

    added binary model + train

--- backend/algorithms/neural_networks.py
+++ backend/algorithms/neural_networks.py
@@ -1,8 +1,10 @@
-from typing import Optional
+from typing import List, Optional, Tuple
+import tensorflow as tf
 from tensorflow import keras
 from enum import Enum
-
+import numpy as np
 from tensorflow.python.framework.ops import pack_eager_tensors
+from tensorflow.python.keras.engine.base_layer import Layer
 
 class ModelTypes(Enum):
     binary = 1
@@ -18,37 +20,84 @@ class ActivationFunctions(Enum):
 
 class ClassificationModel:
     
-    def __init__(self, type : Optional[ModelTypes] = None, layers : int = 1 , neurons:int = 10, input_dim: int = 1, output_dim: int  = 2, act_func: Optional[ActivationFunctions]= None, hidden_act_func: Optional[ActivationFunctions]= None):
+    def __init__(self, type : Optional[ModelTypes] = None,
+    layers : int = 1 ,
+    neurons:int = 10,
+    input_shape: tuple = (1,),
+    output_dim: int  = 1,
+    act_func: Optional[ActivationFunctions]= None,
+    hidden_act_func: Optional[ActivationFunctions]= None,
+    dropout : int = None):
         self.type = type
         self.layers = layers
         self.neurons = neurons
-        self.input_dim = input_dim
+        self.input_shape = input_shape
         self.output_dim = output_dim
         self.act_func = act_func
         self.hidden_act_func = hidden_act_func
+        self.dropout = dropout
+        self.model = None
 
-    @classmethod
+
     def create(self):
         #TODO: exception if not all necessary values was given in init 
         pass
-
-    @classmethod
-    def binary(self):
-        pass
     
-    @classmethod
+    def binary(self):
+        '''Creates a sequential keras model'''
+        self.model = keras.Sequential()
+
+        self.model.add(keras.layers.Dense(self.neurons, activation= 'relu', input_shape = self.input_shape)) #input layer
+        for i in range(self.layers - 1):
+            self.model.add(keras.layers.Dense(self.neurons, activation= 'relu')) #hidden layers
+            if self.dropout:
+                self.model.add(keras.layers.Dropout(0.1))
+        self.model.add(keras.layers.Dense(1, activation= 'sigmoid')) #output layer
+        self.type = ModelTypes.binary
+        
+    
     def multi_class(self):
         pass
     
-    @classmethod
     def multi_label(self):
         pass
 
-    def train():
+    def train(self, train_x: np.ndarray, train_y: np.ndarray,
+            batch_size:int = 128, val_split = 0.1, epochs: int = 20, early_stopping: bool = True): #data_x is a [[1,2,3,4 ... some data]] and data_y = [0] thi is the label
+        '''trains the model'''
+        
+        self.model.compile(optimizer = 'adam', loss='binary_crossentropy')
+        if early_stopping:
+            callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=5)
+            history = self.model.fit(train_x, train_y,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_split= val_split,
+                        shuffle=True,
+                        verbose=1,
+                        callbacks = [callback])
+        else:
+            history = self.model.fit(train_x, train_y,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_split= val_split,
+                        shuffle=True,
+                        verbose=1)
+
+        return history
+
+    def save_model(self, path):
         pass
 
-    def predict():
+    def load_model(self, path):
+        pass
+
+    def predict(self, data):
         pass
 
 if __name__ == '__main__':
-    pass
+    train_x, train_y = np.arange(100).reshape(5, 20), np.zeros(5)
+    model = ClassificationModel(layers= 10, neurons= 20, input_shape= (train_x.shape[-1],))
+    model.binary()
+    hist = model.train(train_x,train_y, batch_size=3,val_split=0.2,epochs= 10, early_stopping=False)
+    print(hist)
