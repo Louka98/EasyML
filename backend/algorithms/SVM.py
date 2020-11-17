bb27ca2906e322da924c9481006f82391c7c57f2
@Louka98 <loubna.kadri@yahoo.com>
2020-11-17 01:53:44 +0100

    Working visualization for SVM

--- backend/algorithms/SVM.py
+++ backend/algorithms/SVM.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
-
+from sklearn import datasets
 #add upload dataset function
 
 class SVM: 
@@ -16,7 +17,7 @@ class SVM:
        self.model = SVC(kernel=self.kernel, gamma = self.gamma, C= self.C, shrinking = self.shrinking)
 
 
-    @classmethod
+    
     def train(self , X_train, y_train):
     #    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = self.test_size)
        self.model = self.model.fit(X_train, y_train)   # à continuer d'ici
@@ -24,11 +25,10 @@ class SVM:
        
 
 
-    @classmethod
     def predict(self, X_test, y_test):
         y_pred = self.model.predict(X_test)
         return classification_report(y_test,y_pred) , (confusion_matrix(y_test,y_pred))
-    @classmethod
+
     def visualize(self , X_train):
         support_vectors = self.model.support_vectors_
         plt.scatter(X_train[:,0], X_train[:,1])
@@ -38,6 +38,20 @@ class SVM:
         plt.ylabel('X2')
         plt.show()
 
+if __name__ == "__main__":    
+    
+    # wine = datasets.load_wine()
+    # model = SVM(C = 0.01 , kernel ='linear', gamma = 0.1,shrinking=False)
+    # dataset = pd.DataFrame(np.c_[wine['data'], wine['target']],
+    #                  columns= wine['feature_names'] + ['target'])
+    
+    # X = dataset.iloc[:, :-1].values
+    # y = dataset.iloc[:, -1].values
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+    # # model.train(X_train,y_train) 
+    # # model.predict(X_test,y_test)  
+    # # model.visualize(X_train)  
 
 
 
