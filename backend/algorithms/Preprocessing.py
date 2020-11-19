dab2db9d90a423c2e86e7a76360daf40759f79e3
samir-99 <62739013+samir-99@users.noreply.github.com>
2020-11-19 21:08:52 +0200

    Add files via upload

--- backend/algorithms/Preprocessing.py
+++ backend/algorithms/Preprocessing.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 class preprocess:
     def __init__(self):
@@ -20,3 +22,13 @@ class preprocess:
                 data[x]=((data[x]-data[x].mean())/(data[x].std()))
         return data
 
+    def onehotencoder(data,categories):
+       
+        categories= categories
+        one_hot=OneHotEncoder()
+        transformer= ColumnTransformer([("one_hot",one_hot,categories)],remainder='passthrough')
+        transformed_X= transformer.fit_transform(data)
+        transformed_X=pd.DataFrame(transformed_X)
+        return transformed_X
+    
+    
