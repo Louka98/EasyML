89fed999aba6e832acdb158f861db9852d2bd921
Czapp Adam <czappadam@gmail.com>
2020-11-30 18:04:37 +0100

    added plotting

--- backend/backend/general_model.py
+++ backend/backend/general_model.py
@@ -1,6 +1,11 @@
 from backend.neural_networks import *
 from algorithms.Clusters import clustering
 import traceback
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from numpy.lib.type_check import imag
 
 cluster_alg_names = ['kmeans']
 
@@ -56,3 +61,47 @@ def train_model(model,train_x,train_y,test_x,test_y,**kwargs):
         traceback.print_exc()
         
         return None
+
+def create_plot(model_type:str, hist):
+    
+    figsize = 5
+
+    if model_type == 'nn_custom':
+        fig = Figure(figsize=(figsize* 1.3,figsize*2))
+        canvas = FigureCanvas(fig)
+
+        print(hist)
+
+        x = np.linspace(0, len(hist['loss']) -1, num = len(hist['loss']), dtype= np.int)
+
+        loss = fig.add_subplot(2,1,1)
+        loss.set_title('Loss')
+        loss.plot(x, hist['loss'], label = 'loss', color = 'red')
+        loss.plot(x, hist['val_loss'], label = 'val_loss', color = 'blue')
+        loss.set_xlabel('epochs')
+        loss.set_ylabel('loss')
+        loss.legend()
+
+        acc = fig.add_subplot(2,1,2)
+        acc.set_title('Accuracy')
+        acc.plot(x, hist['acc'], label = 'accuracy', color = 'red')
+        acc.plot(x, hist['val_acc'], label = 'validation accuracy', color = 'blue')
+        acc.set_xlabel('epochs')
+        acc.set_ylabel('accuracy')
+        acc.legend()
+        #fig.subplots_adjust(hspace = 0.3)
+
+        canvas.draw()       # draw the canvas, cache the renderer
+
+        width, height = fig.get_size_inches() * fig.get_dpi()
+        width, height = int(width), int(height)
+        image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(height,width,3)
+        print('IMAGE_______')
+        #print(image)
+        print(image.shape)
+        print(width)
+        print(height)
+        #plt.imshow(image)
+        #plt.show()
+
+    return image, height, width
