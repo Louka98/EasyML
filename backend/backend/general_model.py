8a5c0604830b3875fdf06806e4b342aa8383bcd3
Czapp Adam <czappadam@gmail.com>
2020-12-01 23:04:21 +0100

    added visualization for clustering

--- backend/backend/general_model.py
+++ backend/backend/general_model.py
@@ -6,6 +6,7 @@ import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 from numpy.lib.type_check import imag
+import seaborn as sns
 
 cluster_alg_names = ['kmeans']
 
@@ -25,7 +26,11 @@ def init_model(**kwargs):
 
             model = clustering()
             if kwargs['model_type'] == 'kmeans':
-                n_clusters = model.nclusters(kwargs['dataset'])                
+                try:
+                    n_clusters = kwargs['n_clusters']
+                except KeyError:
+                    n_clusters, sil_scores, wcss = model.nclusters(kwargs['dataset'])
+
                 model.kmeans(n_clusters=n_clusters)
 
         if kwargs['model_type'] == 'nn_binary_classification':
@@ -62,7 +67,7 @@ def train_model(model,train_x,train_y,test_x,test_y,**kwargs):
         
         return None
 
-def create_plot(model_type:str, hist):
+def create_plot(model_type:str, hist, data = None):
     
     figsize = 5
 
@@ -104,4 +109,58 @@ def create_plot(model_type:str, hist):
         #plt.imshow(image)
         #plt.show()
 
-    return image, height, width
+        return image.tolist(), height, width
+
+    if model_type == 'kmeans':
+        hist['labels']
+        data
+        
+        fig = Figure(figsize=(figsize* 1.3,figsize*2))
+        canvas = FigureCanvas(fig)
+        x = np.linspace(0, len(hist['sil_score']) -1, num = len(hist['sil_score']), dtype= np.int)
+
+        sil_score = fig.add_subplot(2,1,1)
+        sil_score.set_title('Silhouette scores')
+        sil_score.plot(x, hist['sil_score'], label = 'sil score', color = 'red')
+        sil_score.set_xlabel('number of clusters')
+        sil_score.set_ylabel('silhouette score')
+        sil_score.legend()
+
+        wcss = fig.add_subplot(2,1,2)
+        wcss.set_title('Elbow Method Graph')
+        wcss.plot(x, hist['wcss'], label = 'wcss', color = 'red')
+        wcss.set_xlabel('number of clusters')
+        wcss.set_ylabel('WCSS')
+        wcss.legend()
+        #fig.subplots_adjust(hspace = 0.3)
+
+        canvas.draw()       # draw the canvas, cache the renderer
+
+        width, height = fig.get_size_inches() * fig.get_dpi()
+        width, height = int(width), int(height)
+        image = np.fromstring(canvas.tostring_rgb(), dtype='uint8').reshape(height,width,3)
+        print('IMAGE_______')
+        #print(image)
+        print(image.shape)
+        print(width)
+        print(height)
+        #plt.imshow(image)
+        #plt.show()
+
+        pairplot = sns.pairplot(data, hue = 'labels')
+        fig2 = pairplot.fig
+        fig2.set_size_inches(12,10)
+        canvas2 = FigureCanvas(fig2)
+        canvas2.draw()
+        width2, height2 = fig2.get_size_inches() * fig2.get_dpi()
+        width2, height2 = int(width2), int(height2)
+        image2 = np.fromstring(canvas2.tostring_rgb(), dtype='uint8').reshape(height2,width2,3)
+        print('IMAGE_______')
+        print(image2.shape)
+        print(width2)
+        print(height2)
+
+        # plt.imshow(image2)
+        # plt.show()
+
+        return image.tolist(), height, width, image2.tolist(), height2, width2,
